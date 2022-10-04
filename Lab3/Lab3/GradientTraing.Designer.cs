3d08dd587b575ba031bd278f4aab6523d3b7f0cd
Kekuz <anton.edlenko@yandex.ru>
2022-10-05 01:26:43 +0300

    Мощный дизигн

--- Lab3/Lab3/GradientTraing.Designer.cs
+++ Lab3/Lab3/GradientTraing.Designer.cs
@@ -34,69 +34,84 @@
             this.color3 = new System.Windows.Forms.Button();
             this.Clear = new System.Windows.Forms.Button();
             this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
             this.SuspendLayout();
             // 
             // canvas
             // 
             this.canvas.BackColor = System.Drawing.Color.White;
-            this.canvas.Location = new System.Drawing.Point(9, 8);
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(10, 9);
+            this.canvas.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.canvas.Name = "canvas";
-            this.canvas.Size = new System.Drawing.Size(325, 399);
+            this.canvas.Size = new System.Drawing.Size(379, 460);
             this.canvas.TabIndex = 0;
             this.canvas.TabStop = false;
             this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
             // 
             // color1
             // 
-            this.color1.Location = new System.Drawing.Point(351, 8);
+            this.color1.Location = new System.Drawing.Point(397, 46);
+            this.color1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.color1.Name = "color1";
-            this.color1.Size = new System.Drawing.Size(73, 84);
+            this.color1.Size = new System.Drawing.Size(149, 47);
             this.color1.TabIndex = 1;
-            this.color1.Text = "color1";
             this.color1.UseVisualStyleBackColor = true;
             this.color1.Click += new System.EventHandler(this.color1_Click);
             // 
             // color2
             // 
-            this.color2.Location = new System.Drawing.Point(351, 98);
+            this.color2.Location = new System.Drawing.Point(397, 99);
+            this.color2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.color2.Name = "color2";
-            this.color2.Size = new System.Drawing.Size(73, 80);
+            this.color2.Size = new System.Drawing.Size(149, 47);
             this.color2.TabIndex = 2;
-            this.color2.Text = "color2";
             this.color2.UseVisualStyleBackColor = true;
             this.color2.Click += new System.EventHandler(this.color2_Click);
             // 
             // color3
             // 
-            this.color3.Location = new System.Drawing.Point(351, 184);
+            this.color3.Location = new System.Drawing.Point(397, 152);
+            this.color3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.color3.Name = "color3";
-            this.color3.Size = new System.Drawing.Size(73, 81);
+            this.color3.Size = new System.Drawing.Size(149, 47);
             this.color3.TabIndex = 3;
-            this.color3.Text = "color3";
             this.color3.UseVisualStyleBackColor = true;
             this.color3.Click += new System.EventHandler(this.color3_Click);
             // 
             // Clear
             // 
-            this.Clear.Location = new System.Drawing.Point(351, 366);
+            this.Clear.Location = new System.Drawing.Point(397, 423);
+            this.Clear.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Clear.Name = "Clear";
-            this.Clear.Size = new System.Drawing.Size(73, 40);
+            this.Clear.Size = new System.Drawing.Size(149, 46);
             this.Clear.TabIndex = 4;
-            this.Clear.Text = "Clear";
+            this.Clear.Text = "Отчистить";
             this.Clear.UseVisualStyleBackColor = true;
             this.Clear.Click += new System.EventHandler(this.Clear_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(410, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Цвета треугольника";
+            // 
             // GradientTraing
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 415);
+            this.ClientSize = new System.Drawing.Size(559, 482);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Clear);
             this.Controls.Add(this.color3);
             this.Controls.Add(this.color2);
             this.Controls.Add(this.color1);
             this.Controls.Add(this.canvas);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "GradientTraing";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -104,6 +119,8 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.GradientTraing_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         private System.Windows.Forms.ColorDialog colorDialog;
@@ -115,5 +132,7 @@
         private System.Windows.Forms.Button Clear;
 
         #endregion
+
+        private Label label1;
     }
 }
