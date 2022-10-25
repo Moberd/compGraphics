67298be07e71c24fbc81f17beec813054e1d74de
josephjoguts <josephjoguts@yandex.ru>
2022-10-25 20:23:35 +0300

    mountains

--- Lab5/Lab5/FormMountains.Designer.cs
+++ Lab5/Lab5/FormMountains.Designer.cs
@@ -30,33 +30,142 @@ namespace Lab5
         private void InitializeComponent()
         {
             this.canvas = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // canvas
             // 
             this.canvas.Location = new System.Drawing.Point(0, 0);
-            this.canvas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.canvas.Name = "canvas";
-            this.canvas.Size = new System.Drawing.Size(682, 325);
+            this.canvas.Size = new System.Drawing.Size(910, 500);
             this.canvas.TabIndex = 0;
             this.canvas.TabStop = false;
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(23, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать границы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Коэф. шероховатости";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Рисовать горы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 302);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Глубина рекурсии";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(23, 175);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(130, 56);
+            this.trackBar1.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0,1",
+            "0,2",
+            "0,3",
+            "0,4",
+            "0,5",
+            "0,6",
+            "0,7",
+            "0,8",
+            "0,9",
+            "1",
+            "1,1",
+            "1,2",
+            "1,3",
+            "1,4",
+            "1,5",
+            "1,6",
+            "1,7",
+            "1,8",
+            "1,9",
+            "2"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 100);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 8;
             // 
             // FormMountains
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(681, 325);
+            this.ClientSize = new System.Drawing.Size(908, 500);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.canvas);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormMountains";
             this.Text = "FormMountains";
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Lab5/Lab5/FormMountains.cs
+++ Lab5/Lab5/FormMountains.cs
@@ -10,13 +10,126 @@ using System.Windows.Forms;
 
 namespace Lab5
 {
+
+
     public partial class FormMountains : Form
     {
-        public FormMountains()
-        //Реализовать алгоритм midpoint displacement для двумерной визуализации горного массива.
+        bool drawingMode;
+        double n;
+        double r;
+        public FormMountains() 
         {
-            //Здесь пишет Данил
             InitializeComponent();
+            trackBar1.Scroll += trackBar1_Scroll;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            g = canvas.CreateGraphics();
+            canvas.Image = new Bitmap(1300, 900);
+            drawingMode = false;
+        }
+        List<Point> polygonPoints = new List<Point>();
+        List<segment> Mountains = new List<segment>();
+        
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        Pen blackPen = new Pen(Color.Black, 3);
+        Pen blackPen2 = new Pen(Color.Black, 1);
+        Graphics g;
+
+        void drawPoint(MouseEventArgs e)
+        {
+
+
+            polygonPoints.Add(e.Location);
+            if (polygonPoints.Count == 2)
+                Mountains.Add(new segment(polygonPoints[0], polygonPoints[1]));
+            g.FillEllipse(blackBrush, e.X - 3, e.Y - 3, 7, 7);
+
+        }
+        void canvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (drawingMode)
+            {
+
+                drawPoint(e);
+
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawingMode = true;
+        }
+        void DrawMountain()
+        {
+            g.Clear(Color.White);
+            r = double.Parse(comboBox1.SelectedItem.ToString());//коэфф шероховатости зададим сами
+                                            // n= double.Parse(textBox2.Text);//глубина рекурсии задается пользователем
+            n = trackBar1.Value;
+
+
+            for (int j = 1; j <= (int)n; j++)
+            {
+                Random rnd = new Random();
+                List<segment> drawpoints = new List<segment>();
+                foreach (segment x in Mountains)
+                {
+                    Point left = x.l;
+                    Point right = x.r;
+                     double i = Math.Abs(left.X - right.X)* Math.Abs(left.X - right.X) + Math.Abs(left.Y - right.Y) * Math.Abs(left.Y - right.Y);
+                    i = Math.Sqrt(i);
+                    int c = (left.X + right.X) / 2;
+                    double y = (left.Y + right.Y) / 2 + (rnd.NextDouble() - 0.5)*r * i;
+                    Point center = new Point(c, (int)y);
+                    drawpoints.Add(new segment(left, center));
+                    drawpoints.Add(new segment(center, right));
+                }
+                Mountains = drawpoints;
+            }
+            DrawMountainsHelp();
+        }
+        void DrawSegment(segment s)
+        {
+            g.DrawLine(blackPen2, s.l, s.r);
+        }
+        void DrawMountainsHelp()
+        {
+            foreach (segment x in Mountains)
+                DrawSegment(x);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DrawMountain();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+                polygonPoints.Clear();
+                Mountains.Clear();
+
+                drawingMode = false;
+
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = String.Format("Глубина рекурсии: {0}", trackBar1.Value);
+        }
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedState = comboBox1.SelectedItem.ToString();
         }
     }
+    class segment
+    {
+        public Point l;
+        public Point r;
+        public
+        segment(Point p1, Point p2)
+        {
+            l = p1;
+            r = p2;
+        }
+
+    };
 }
