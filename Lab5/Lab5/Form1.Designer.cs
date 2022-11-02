fe99c2336d6f8dcac1ff24c3f7dad74e72fef6de
Kekuz <anton.edlenko@yandex.ru>
2022-11-02 04:27:06 +0300

    Кривые Безье сделаны + перерисован дизайн главного меню

--- Lab5/Lab5/Form1.Designer.cs
+++ Lab5/Lab5/Form1.Designer.cs
@@ -36,34 +36,34 @@ namespace Lab5
             // 
             // buttonFrac
             // 
-            this.buttonFrac.Location = new System.Drawing.Point(53, 40);
-            this.buttonFrac.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonFrac.Location = new System.Drawing.Point(53, 30);
+            this.buttonFrac.Margin = new System.Windows.Forms.Padding(2);
             this.buttonFrac.Name = "buttonFrac";
-            this.buttonFrac.Size = new System.Drawing.Size(136, 32);
+            this.buttonFrac.Size = new System.Drawing.Size(136, 70);
             this.buttonFrac.TabIndex = 0;
-            this.buttonFrac.Text = "Рисовать фракталы";
+            this.buttonFrac.Text = "Фрактальные узоры";
             this.buttonFrac.UseVisualStyleBackColor = true;
             this.buttonFrac.Click += new System.EventHandler(this.buttonFrac_Click);
             // 
             // buttonMountains
             // 
-            this.buttonMountains.Location = new System.Drawing.Point(53, 74);
-            this.buttonMountains.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonMountains.Location = new System.Drawing.Point(53, 104);
+            this.buttonMountains.Margin = new System.Windows.Forms.Padding(2);
             this.buttonMountains.Name = "buttonMountains";
-            this.buttonMountains.Size = new System.Drawing.Size(136, 32);
-            this.buttonMountains.TabIndex = 0;
-            this.buttonMountains.Text = "Рисовать горы";
+            this.buttonMountains.Size = new System.Drawing.Size(136, 70);
+            this.buttonMountains.TabIndex = 1;
+            this.buttonMountains.Text = "Горный массив";
             this.buttonMountains.UseVisualStyleBackColor = true;
             this.buttonMountains.Click += new System.EventHandler(this.buttonMountains_Click);
             // 
             // buttonBezier
             // 
-            this.buttonBezier.Location = new System.Drawing.Point(53, 109);
-            this.buttonBezier.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonBezier.Location = new System.Drawing.Point(53, 178);
+            this.buttonBezier.Margin = new System.Windows.Forms.Padding(2);
             this.buttonBezier.Name = "buttonBezier";
-            this.buttonBezier.Size = new System.Drawing.Size(136, 32);
-            this.buttonBezier.TabIndex = 0;
-            this.buttonBezier.Text = "Рисовать кривые Безье";
+            this.buttonBezier.Size = new System.Drawing.Size(136, 70);
+            this.buttonBezier.TabIndex = 2;
+            this.buttonBezier.Text = "Кривые Безье";
             this.buttonBezier.UseVisualStyleBackColor = true;
             this.buttonBezier.Click += new System.EventHandler(this.buttonBezier_Click);
             // 
@@ -71,14 +71,17 @@ namespace Lab5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(238, 163);
+            this.ClientSize = new System.Drawing.Size(238, 273);
             this.Controls.Add(this.buttonBezier);
             this.Controls.Add(this.buttonMountains);
             this.Controls.Add(this.buttonFrac);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Сделай выбор, Нео";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Выбор задания";
             this.ResumeLayout(false);
+
         }
 
         #endregion
