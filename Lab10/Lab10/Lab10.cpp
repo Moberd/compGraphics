b057930a269f861059c9e9880436c89cba251fb1
Kekuz <anton.edlenko@yandex.ru>
2022-12-14 01:24:02 +0300

    Lab 10 is done)

---
+++ Lab10/Lab10/Lab10.cpp
@@ -0,0 +1,197 @@
+﻿#include <iostream>
+#include <gl/glew.h>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+// ID шейдерной программы
+GLuint Program;
+// ID атрибута
+GLint Attrib_vertex;
+// ID Vertex Buffer Object
+GLuint VBO;
+
+struct Vertex {
+	GLfloat x;
+	GLfloat y;
+};
+
+// Исходный код вершинного шейдера
+const char* VertexShaderSource = R"(
+	#version 330 core
+	in vec2 coord;
+	void main() {
+		gl_Position = vec4(coord, 0.0, 1.0);
+	}
+)";
+
+// Исходный код фрагментного шейдера
+const char* FragShaderSource = R"(
+	#version 330 core
+	out vec4 color;
+	void main() {
+		color = vec4(0, 1, 0, 1);
+	}
+)";
+
+
+//void SetIcon(sf::Window& wnd);
+void Init();
+void Draw();
+
+// Освобождение буфера
+void ReleaseVBO() {
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VBO);
+}
+
+// Освобождение шейдеров
+void ReleaseShader() {
+	// Передавая ноль, мы отключаем шейдерную программу
+	glUseProgram(0);
+	// Удаляем шейдерную программу
+	glDeleteProgram(Program);
+}
+
+void Release() {
+	// Шейдеры
+	ReleaseShader();
+	// Вершинный буфер
+	ReleaseVBO();
+}
+
+int main() {
+	sf::Window window(sf::VideoMode(600, 600), "My OpenGL window",
+		sf::Style::Default, sf::ContextSettings(24));
+	window.setVerticalSyncEnabled(true);
+	window.setActive(true);
+	glewInit();
+	Init();
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) { window.close(); }
+			else if (event.type == sf::Event::Resized) {
+				glViewport(0, 0, event.size.width,
+					event.size.height);
+			}
+		}
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		Draw();
+		window.display();
+	}
+	Release();
+
+	return 0;
+}
+
+void checkOpenGLerror()
+{
+	GLenum err;
+	while ((err = glGetError()) != GL_NO_ERROR)
+	{
+		// Process/log the error.
+		std::cout << err << std::endl;
+	}
+}
+void ShaderLog(unsigned int shader)
+{
+	int infologLen = 0;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infologLen);
+	if (infologLen > 1)
+	{
+		int charsWritten = 0;
+		std::vector<char> infoLog(infologLen);
+		glGetShaderInfoLog(shader, infologLen, &charsWritten, infoLog.data());
+		std::cout << "InfoLog: " << infoLog.data() << std::endl;
+	}
+}
+
+void InitShader() {
+	// Создаем вершинный шейдер
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	// Передаем исходный код
+	glShaderSource(vShader, 1, &VertexShaderSource, NULL);
+	// Компилируем шейдер
+	glCompileShader(vShader);
+	std::cout << "vertex shader \n";
+	// Функция печати лога шейдера
+	ShaderLog(vShader); //Пример функции есть в лабораторной
+
+	// Создаем фрагментный шейдер
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+	// Передаем исходный код
+	glShaderSource(fShader, 1, &FragShaderSource, NULL);
+	// Компилируем шейдер
+	glCompileShader(fShader);
+	std::cout << "fragment shader \n";
+	// Функция печати лога шейдера
+	ShaderLog(fShader);
+
+	// Создаем программу и прикрепляем шейдеры к ней
+	Program = glCreateProgram();
+	glAttachShader(Program, vShader);
+	glAttachShader(Program, fShader);
+	// Линкуем шейдерную программу
+	glLinkProgram(Program);
+	// Проверяем статус сборки
+	int link_ok;
+	glGetProgramiv(Program, GL_LINK_STATUS, &link_ok);
+	if (!link_ok) {
+		std::cout << "error attach shaders \n";
+		return;
+	}
+	// Вытягиваем ID атрибута из собранной программы
+	const char* attr_name = "coord"; //имя в шейдере
+	Attrib_vertex = glGetAttribLocation(Program, attr_name);
+	if (Attrib_vertex == -1) {
+		std::cout << "could not bind attrib " << attr_name << std::endl;
+		return;
+	}
+	checkOpenGLerror();
+}
+
+void InitVBO() {
+	glGenBuffers(1, &VBO);
+	// Вершины нашего треугольника
+	Vertex triangle[3] = {
+	{ -1.0f, -1.0f },
+	{ 0.0f, 1.0f },
+	{ 1.0f, -1.0f }
+	};
+	// Передаем вершины в буфер
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+	checkOpenGLerror(); //Пример функции есть в лабораторной
+	// Проверка ошибок OpenGL, если есть то вывод в консоль тип ошибки
+}
+
+void Draw() {
+	// Устанавливаем шейдерную программу текущей
+	glUseProgram(Program);
+	// Включаем массив атрибутов
+	glEnableVertexAttribArray(Attrib_vertex);
+	// Подключаем VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Указывая pointer 0 при подключенном буфере, мы указываем что данные в VBO
+	glVertexAttribPointer(Attrib_vertex, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	// Отключаем VBO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// Передаем данные на видеокарту(рисуем)
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	// Отключаем массив атрибутов
+	glDisableVertexAttribArray(Attrib_vertex);
+	// Отключаем шейдерную программу
+	glUseProgram(0);
+	checkOpenGLerror();
+}
+
+// В момент инициализации разумно произвести загрузку текстур, моделей и других вещей
+void Init() {
+	// Шейдеры
+	InitShader();
+	//
+
+	//Вершинный буфер
+	InitVBO();
+}
